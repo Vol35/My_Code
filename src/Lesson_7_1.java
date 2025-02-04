e4a8fea33d46bb05db3d643fee37cf7680edb357
Vol35 <140203124+Vol35@users.noreply.github.com>
2025-02-04 22:25:09 +0300

    add new lessons

--- src/Lesson_7_1.java
+++ src/Lesson_7_1.java
@@ -1,8 +1,17 @@
 public class Lesson_7_1 {
     public static void main(String[] args) {
         //todo Тут увольняем айтишников
-        for (int i=0; i<=100;i=i+2){
+        for (int i=2; i<=100; i++){
+            if (i % 2 == 0){
+                System.out.println("Уволен айтишник с id = " + i);
+
+        /* - Второй вариант:
+        for (int i=2; i<=100;i=i+2){
             System.out.println("Уволен айтишник с id = " + i);
         }
+        */
+            }
+        }
+
     }
 }
