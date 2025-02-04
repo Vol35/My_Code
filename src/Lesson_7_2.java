92705f6b963fc0f21bb43d626db651c59c091509
Vol35 <140203124+Vol35@users.noreply.github.com>
2025-02-04 22:26:43 +0300

    add new lessons

--- src/Lesson_7_2.java
+++ src/Lesson_7_2.java
@@ -1,7 +1,7 @@
 public class Lesson_7_2 {
     public static void main(String[] args) {
         //todo Тут увольняем айтишников
-        int idPerson = 0;
+        int idPerson = 2;
         while (idPerson <= 100) {
             System.out.println("Уволен айтишник с id = " + idPerson);
             idPerson = idPerson +2;
