3912148770a9f7751ff83b69aa0d9370dca41cbd
Vol35 <140203124+Vol35@users.noreply.github.com>
2025-02-25 21:27:20 +0300

    last commit

--- src/Lesson_11.java
+++ src/Lesson_11.java
@@ -1,4 +1,7 @@
 public class Lesson_11 {
+  public static void main(String[] args) {
+    receiveCall();
+  }
 
 }
 
--- src/Phone.java
+++ src/Phone.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
+
 public class Phone {
     static String country = "";
     int numberPhone;
     String modelPhone;
     double weightPhone;
 
+    // Конструктор пустой
+    public Phone(){
+}
+    // Конструктор со всеми полями класса
+    public Phone (int numberPhone, String modelPhone, double weightPhone){
+        this.numberPhone = numberPhone;
+        this.modelPhone = modelPhone;
+        this.weightPhone = weightPhone;
+}
+    // Метод класса
+    public void receiveCall(){
+        System.out.println("Имя звонящего = ");
+        new Scanner(System.in).nextInt();
+}
 
 }
